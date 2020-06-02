e1418126932909b46395ad9a8e7e3aa03c4452d4
Ehlert, Alexander Z <alexander.ehlert@pnnl.gov>
2020-06-01 17:41:21 -0700

    Added the PSI PERM parser

---
+++ SPEAR/Models/Devices/PsiPerm.cs
@@ -0,0 +1,25 @@
+﻿using SPEAR.Parsers.Devices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPEAR.Models.Devices
+{
+    public class PsiPerm : DeviceInfo
+    {
+        /////////////////////////////////////////////////////////////////////////////////////////
+        // Constructor
+        /////////////////////////////////////////////////////////////////////////////////////////
+        public PsiPerm()
+        {
+            // Set defaults
+            DeviceTypeEnum = Type.PsiPerm;
+
+            SupportedFileExts = new List<FileExt>() {
+                new FileExt() { FileExtName = "*.XML", FileExtType = FileExt.Type.N42, FileParser = new PsiPermN42Parser() }
+            };
+        }
+    }
+}
